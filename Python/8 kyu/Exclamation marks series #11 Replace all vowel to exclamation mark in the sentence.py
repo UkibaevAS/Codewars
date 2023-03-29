937a0aefaff7ed50448c6abe87f63c46150482a8
UkibaevAS <Ukibaev_AS@mail.ru>
2023-03-29 08:09:27 +0500

    done

---
+++ Python/8 kyu/Exclamation marks series #11 Replace all vowel to exclamation mark in the sentence.py
@@ -0,0 +1,15 @@
+"""
+DESCRIPTION:
+Description:
+Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+Examples
+replace("Hi!") === "H!!"
+replace("!Hi! Hi!") === "!H!! H!!"
+replace("aeiou") === "!!!!!"
+replace("ABCDE") === "!BCD!"
+"""
+
+
+def replace_exclamation(s):
+    return ''.join('!' if i.lower() in "aeiou" else i for i in s)

fa0246e337204d89fb7e0f9d2bd2444e7806bf82
UkibaevAS <Ukibaev_AS@mail.ru>
2023-05-03 22:10:48 +0500

    done

---
+++ Python/7 kyu/Double value every next call.py
@@ -0,0 +1,17 @@
+"""
+This kata is about static method that should return different values.
+
+On the first call it must be 1, on the second and others - it must be a double from previous value.
+
+Look tests for more examples, good luck :)
+"""
+
+
+class Class:
+    num = 1
+
+    @staticmethod
+    def get_number():
+        result = Class.num
+        Class.num *= 2
+        return result

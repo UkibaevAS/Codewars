7bd7e78b824117b5b912641217a87468d305664e
UkibaevAS <Ukibaev_AS@mail.ru>
2023-12-09 04:19:32 +0500

    done

---
+++ Python/7 kyu/Alternate case.py
@@ -0,0 +1,8 @@
+"""
+Write function alternateCase which switch every letter in string from upper to lower and from lower to upper.
+E.g: Hello World -> hELLO wORLD
+"""
+
+
+def alternate_case(s):
+    return s.swapcase()
